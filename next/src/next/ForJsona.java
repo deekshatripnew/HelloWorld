7505c1d3edf665d6026866763ecbc866d07082a7
deeks <deeks@LAPTOP-GNQF81UB.localdomain>
2020-08-04 07:40:21 +0530

    test

---
+++ next/src/next/ForJsona.java
@@ -0,0 +1,5 @@
+package next;
+
+public class ForJsona {
+
+}
---
+++ next/src/next/MyMy.java
@@ -0,0 +1,31 @@
+package next;
+
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
+
+
+public class MyMy {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		String line = "";
+		// Gson gson = new Gson();
+		 ArrayList<String> l1=new ArrayList<>();
+	        ArrayList<String> l2=new ArrayList<>();
+		 BufferedReader br = new BufferedReader(new FileReader("./json.txt"));
+			while ((line = br.readLine()) != null) {
+	           String[] transactionAttributes = line.split(",");
+				//my2 f=gson.fromJson(line, my2.class);			
+	            l1.add(transactionAttributes[0]);
+	            l2.add(transactionAttributes[1]);
+
+	}
+			System.out.println(l1+" "+l2);
+
+}
+}
